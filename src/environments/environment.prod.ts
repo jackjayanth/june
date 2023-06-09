1d0d8111ddd93ea01be0fd1e3098f03a33c577d8
Jay <jayanth.rymec@gmail.com>
2023-06-08 23:10:21 -0600

    adding firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'june-82fc9',
+    appId: '1:464993928436:web:caecb2b512f1f1e059caa1',
+    storageBucket: 'june-82fc9.appspot.com',
+    apiKey: 'AIzaSyC2dgoEGgS-GT_rNQIM4nvKX09UdPdnw-I',
+    authDomain: 'june-82fc9.firebaseapp.com',
+    messagingSenderId: '464993928436',
+    measurementId: 'G-H109Q6EXYS',
+  },
   production: true
 };
